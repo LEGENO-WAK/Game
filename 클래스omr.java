6e8dda21c374138e70ba7bbc758497e142187d14
osk2090 <osk2090@gmail.com>
2020-11-05 01:16:48 +0900

    omrCard
    no revise

---
+++ 클래스omr.java
@@ -0,0 +1,49 @@
+package 클래스.클래스기본;
+
+import java.util.Random;
+
+class Ex06 {
+    int[] answer = {1, 3, 4, 2, 5,};
+    int[] hgd = new int[5];
+
+    int cnt = 0;
+    int score = 0;
+}
+public class 클래스omr {
+    public static void main(String[] args) {
+        Random ran = new Random();
+        Ex06 a = new Ex06();
+
+        int[] j = new int[5];
+
+        for (int i = 0; i < 5; i++) {
+            a.hgd[i] = ran.nextInt(5) + 1;
+        }
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print(a.answer[i] +"\t");
+        }
+        System.out.println();
+        for (int i = 0; i < 5; i++) {
+            System.out.print(a.hgd[i] + "\t");
+        }
+
+        for (int i = 0; i <5; i++) {
+            if (a.answer[i] == a.hgd[i]) {
+                j[i] = 1;
+                a.cnt += 1;
+            } else if (a.answer[i] == a.hgd[i]) {
+                j[i] = 0;
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < 5; i++) {
+            if (j[i] == 1) {
+                System.out.print("O" + "\t");
+            } else {
+                System.out.print("X" + "\t");
+            }
+        }
+        System.out.println("성적:" + (a.cnt * 20));
+    }
+}
